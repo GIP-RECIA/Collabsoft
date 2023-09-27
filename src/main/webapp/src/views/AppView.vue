57d58fff3eadaafa3b7b9ccc0cc4680548d0e225
Quentin Guillemin <quentin.guillemin@recia.fr>
2023-09-27 12:36:38 +0200

    refactor: AppView base

--- src/main/webapp/src/views/AppView.vue
+++ src/main/webapp/src/views/AppView.vue
@@ -1,5 +1,32 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { getFile } from '@/services/fileService';
+import { ref } from 'vue';
+import { useRoute } from 'vue-router';
 
-<template><div>APP</div></template>
+const route = useRoute();
+
+const file = ref(undefined);
+
+const getData = async () => {
+  const { fileId } = route.params;
+  const result = await getFile(parseInt(fileId as string));
+  file.value = result.data;
+};
+
+getData();
+</script>
+
+<template>
+  <div class="d-flex flex-column h-100">
+    <v-toolbar :title="file?.title" density="compact">
+      <template #prepend>
+        <v-btn density="compact" icon :to="{ name: 'projects' }">
+          <v-icon icon="fas fa-arrow-left" size="xs" />
+        </v-btn>
+      </template>
+    </v-toolbar>
+    {{ file }}
+  </div>
+</template>
 
 <style scoped lang="scss"></style>
