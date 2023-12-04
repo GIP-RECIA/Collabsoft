d283e175891bc9ab02fc4c8ff572433395c438a5
Quentin Guillemin <quentin.guillemin@recia.fr>
2023-12-04 11:55:22 +0100

    feat: redirect home to projects page

--- src/main/webapp/src/views/HomeView.vue
+++ src/main/webapp/src/views/HomeView.vue
@@ -4,6 +4,13 @@ import FileDialog from '@/components/dialogs/FileDialog.vue';
 import SettingsDialog from '@/components/dialogs/SettingsDialog.vue';
 import InformationDrawer from '@/components/drawers/InformationDrawer.vue';
 import NavigationDrawer from '@/components/drawers/NavigationDrawer.vue';
+import { Navigation } from '@/types/enums/Navigation';
+import { useRoute, useRouter } from 'vue-router';
+
+const route = useRoute();
+const router = useRouter();
+
+if (route.name == 'home') router.replace({ name: Navigation.projects });
 </script>
 
 <template>
