75080205655c64e70d940e198eb81e0dcdab8b8f
Quentin Guillemin <quentin.guillemin@recia.fr>
2023-12-05 16:12:53 +0100

    feat: add date-fns utils

---
+++ src/main/webapp/src/utils/dateFnsUtils.ts
@@ -0,0 +1,18 @@
+import { formatDuration, intervalToDuration, parseISO } from 'date-fns';
+
+const dateToDuration = (date: string): string => {
+  const { years, months, weeks, days, hours, minutes, seconds } = intervalToDuration({
+    start: parseISO(date),
+    end: Date.now(),
+  });
+
+  if (years != undefined && years > 0) return formatDuration({ years }, { format: ['years'] });
+  if (months != undefined && months > 0) return formatDuration({ months }, { format: ['months'] });
+  if (weeks != undefined && weeks > 0) return formatDuration({ weeks }, { format: ['weeks'] });
+  if (days != undefined && days > 0) return formatDuration({ days }, { format: ['days'] });
+  if (hours != undefined && hours > 0) return formatDuration({ hours }, { format: ['hours'] });
+  if (minutes != undefined && minutes > 0) return formatDuration({ minutes }, { format: ['minutes'] });
+  return formatDuration({ seconds }, { format: ['seconds'] });
+};
+
+export { dateToDuration };
