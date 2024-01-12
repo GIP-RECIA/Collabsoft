644543a90c75aec19581e90db39e8785accb9ab3
Quentin Guillemin <quentin.guillemin@recia.fr>
2024-01-12 13:17:52 +0100

    refactor: optimize fontawesome imports

--- src/main/webapp/src/plugins/fontawsome.ts
+++ src/main/webapp/src/plugins/fontawsome.ts
@@ -1,11 +1,50 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { far } from '@fortawesome/free-regular-svg-icons';
-import { fas } from '@fortawesome/free-solid-svg-icons';
+import { faStar as farStar } from '@fortawesome/free-regular-svg-icons';
+import {
+  fa1,
+  fa2,
+  faArrowLeft,
+  faCircleInfo,
+  faClockRotateLeft,
+  faCloud,
+  faDownload,
+  faEllipsisVertical,
+  faFolder,
+  faGear,
+  faGlobe,
+  faPlus,
+  faShareNodes,
+  faSortDown,
+  faSortUp,
+  faStar,
+  faTrash,
+  faXmark,
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import type { App } from 'vue';
 
 const register = (app: App): void => {
-  library.add(far, fas);
+  library.add(farStar);
+  library.add(
+    fa1,
+    fa2,
+    faCircleInfo,
+    faClockRotateLeft,
+    faCloud,
+    faDownload,
+    faEllipsisVertical,
+    faFolder,
+    faGear,
+    faGlobe,
+    faPlus,
+    faShareNodes,
+    faSortDown,
+    faSortUp,
+    faStar,
+    faTrash,
+    faXmark,
+    faArrowLeft,
+  );
   app.component('font-awesome-icon', FontAwesomeIcon);
 };
 
