a32942a946714a88fcefa850b185bdd33c797b0f
Quentin Guillemin <quentin.guillemin@recia.fr>
2024-03-18 12:14:57 +0100

    feat(i18n): auto select language based on browser languages order and app available languages

--- src/main/webapp/src/plugins/i18n.ts
+++ src/main/webapp/src/plugins/i18n.ts
@@ -2,15 +2,27 @@ import en from '@/locales/en/index.ts';
 import fr from '@/locales/fr/index.ts';
 import { createI18n } from 'vue-i18n';
 
+const fallbackLocale: string = 'fr';
+const availableLanguages: Array<string> = ['en', 'fr'];
+
+const findLanguage = (): string => {
+  if (availableLanguages.includes(window.navigator.language)) return window.navigator.language;
+
+  const matchLanguages = window.navigator.languages.filter((lang) =>
+    availableLanguages.find((available) => lang == available),
+  );
+  if (matchLanguages.length > 0) return matchLanguages[0];
+
+  return fallbackLocale;
+};
+
 export default createI18n({
   legacy: false,
   allowComposition: true,
-  locale: window.navigator.language,
-  fallbackLocale: 'fr',
+  locale: findLanguage(),
+  fallbackLocale,
   messages: {
     en,
-    'en-US': en,
     fr,
-    'fr-FR': fr,
   },
 });
