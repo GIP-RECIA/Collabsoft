3da391ceead9156d992cb3626a1d0264cb2714d8
Quentin Guillemin <quentin.guillemin@recia.fr>
2024-06-10 14:26:07 +0200

    test: string utils

--- src/test/webapp/spec/utils/stringUtils.spec.ts
+++ src/test/webapp/spec/utils/stringUtils.spec.ts
@@ -1,9 +1,19 @@
 // @ts-ignore
-import { capitalize } from '@/utils/stringUtils';
+import { charOTP, interpolate, slugify } from '@/utils/stringUtils';
 import { describe, expect, it } from 'vitest';
 
 describe('stringUtils', () => {
-  it('capitalize', () => {
-    expect(capitalize('foo')).toBe('Foo');
+  it('charOTP', () => {
+    expect(charOTP()).length(6);
+    expect(charOTP(8)).length(8);
+    expect(charOTP()).not.toBe(charOTP());
+  });
+
+  it('slugify', () => {
+    expect(slugify('Foo Bar --Bar été ')).toBe('foo-bar-bar-ete');
+  });
+
+  it('interpolate', () => {
+    expect(interpolate('foo ${bar}', { bar: 'bar !' })).toBe('foo bar !');
   });
 });
