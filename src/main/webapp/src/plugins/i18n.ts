9878d58fa6dbf1bd3370624122fbcfe889ad4de4
Quentin Guillemin <quentin.guillemin@recia.fr>
2024-08-23 10:29:49 +0200

    refactor(i18n): use index for exports

--- src/main/webapp/src/plugins/i18n.ts
+++ src/main/webapp/src/plugins/i18n.ts
@@ -13,31 +13,32 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import en from '@/locales/en/index.ts';
-import fr from '@/locales/fr/index.ts';
+import * as messages from '@/locales/index.ts';
 import { createI18n } from 'vue-i18n';
 
-const fallbackLocale: string = 'fr';
-const availableLanguages: Array<string> = ['en', 'fr'];
+const config: {
+  fallbackLocale: string;
+  supportLocales: Array<string>;
+} = {
+  fallbackLocale: 'fr',
+  supportLocales: ['en', 'fr'],
+};
 
 const findLanguage = (): string => {
-  if (availableLanguages.includes(window.navigator.language)) return window.navigator.language;
+  if (config.supportLocales.includes(window.navigator.language)) return window.navigator.language;
 
   const matchLanguages = window.navigator.languages.filter((lang) =>
-    availableLanguages.find((available) => lang == available),
+    config.supportLocales.find((available) => lang == available),
   );
   if (matchLanguages.length > 0) return matchLanguages[0];
 
-  return fallbackLocale;
+  return config.fallbackLocale;
 };
 
 export default createI18n({
   legacy: false,
   allowComposition: true,
   locale: findLanguage(),
-  fallbackLocale,
-  messages: {
-    en,
-    fr,
-  },
+  fallbackLocale: config.fallbackLocale,
+  messages,
 });
