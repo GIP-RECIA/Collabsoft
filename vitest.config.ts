aaa4a7f9e21facf2cfa0eaa244151e637dddeb2c
Quentin Guillemin <quentin.guillemin@recia.fr>
2026-03-03 11:03:57 +0100

    fix: eslint issues

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,15 +1,14 @@
-import { fileURLToPath } from 'node:url'
 import { configDefaults, defineConfig, mergeConfig } from 'vitest/config'
-import viteConfig from './vite.config.ts'
+import viteConfig from './vite.config'
 
 export default mergeConfig(
-  viteConfig({ mode: 'test' }),
+  viteConfig({ command: 'serve', mode: 'test' }),
   defineConfig({
     test: {
       globals: true,
+      dir: './src/test/webapp/spec',
       environment: 'jsdom',
       exclude: [...configDefaults.exclude],
-      root: fileURLToPath(new URL('./src/test/webapp/spec', import.meta.url)),
       server: {
         deps: {
           inline: ['vuetify'],
